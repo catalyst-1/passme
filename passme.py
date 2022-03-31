1ecf46bf2cd5d45058066b6b6ab32578f48fffe5
Rammy Sifa <anonrammy@gmail.com>
2022-03-31 16:36:49 +0300

    basic fxn (generate password) done

--- passme.py
+++ passme.py
@@ -1,2 +1,18 @@
-from src import lsfr
-lsfr.lfsr(100)
+from src import *
+import random
+
+All=""
+for i in [alphabet.upper,alphabet.lower,specialchars.specialchars]:
+    for x in i:
+        All += x
+
+run = True
+
+while run:
+    inp =int( input("enter length of your desired password :"))
+    for i in range(inp):
+        j = lsfr.lfsr(random.randint(1000,1000000))%len(All)
+        print(All[j] , end="")
+
+    break
+
