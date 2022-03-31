9562360a0c80d700d75f4227a21427983e64cf6b
Rammy Sifa <anonrammy@gmail.com>
2022-03-31 23:21:48 +0300

    storage and recovery done

--- passme.py
+++ passme.py
@@ -1,18 +1,62 @@
 from src import *
 import random
+import sys
+import pickle
+
+try:
+    open("log","x").close()
+except:
+    pass
 
 All=""
 for i in [alphabet.upper,alphabet.lower,specialchars.specialchars]:
     for x in i:
         All += x
 
-run = True
+try:
+    if sys.argv[1] :
+        pass
+    target = input("enter target : ")
 
-while run:
-    inp =int( input("enter length of your desired password :"))
+    with open('log',"rb") as fd:
+        data = fd.read()
+        base = pickle.loads(data)
+        print(base[target])
+
+except EOFError:
+
+    base ={}
+    base[target] = result
+
+    with open('log','wb') as fd:
+        pickld = pickle.dumps(base)
+        fd.write(pickld)
+
+except IndexError:
+    inp = int(input("enter your passwords desired length : "))
+    target = input("this pasaword is for ? : ")
+    result =""
+
     for i in range(inp):
-        j = lsfr.lfsr(random.randint(1000,1000000))%len(All)
-        print(All[j] , end="")
+        tmp =All[( lsfr.lfsr(random.randint(1000,1000000))%len(All))]
+        result += tmp
+    print(result)
 
-    break
 
+    with open('log','rb') as fd:
+        data = fd.read()
+        try:
+            base = pickle.loads(data)
+            base[target] = result
+        except EOFError:
+
+            base ={}
+            base[target] = result
+
+            with open('log','wb') as fd:
+                pickld = pickle.dumps(base)
+                fd.write(pickld)
+
+    with open('log','wb') as fd:
+        pickld = pickle.dumps(base)
+        fd.write(pickld)
